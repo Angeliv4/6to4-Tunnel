0f5541d831850088a2de83996be445a1a5c24d44
sudo-sa <162730720+sudo-sa@users.noreply.github.com>
2024-07-25 23:22:34 +0330

    Update-10

--- 6to4script.sh
+++ 6to4script.sh
@@ -24,27 +24,23 @@ Enter Number Activity : " act
 		touch /etc/rc.local
 		chmod +x "/etc/rc.local"
 		ip tunnel add 6to4_To_KH mode sit remote $ip_kharej local $ip_iran
-		ip -6 addr add fde8:b030:25cf::de01/64 dev 6to4_To_KH
+		ip -6 addr add fc00::1/64 dev 6to4_To_KH
 		ip link set 6to4_To_KH mtu 1480
 		ip link set 6to4_To_KH up
-		ip -6 tunnel add GRE6Tun_To_KH mode ip6gre remote fde8:b030:25cf::de02 local fde8:b030:25cf::de01
-		ip addr add 172.20.20.1/30 dev GRE6Tun_To_KH
-		ip link set GRE6Tun_To_KH mtu 1436
-		ip link set GRE6Tun_To_KH up
 		echo "#! /bin/bash
 ip tunnel add 6to4_To_KH mode sit remote $ip_kharej local $ip_iran
-ip -6 addr add fde8:b030:25cf::de01/64 dev 6to4_To_KH
+ip -6 addr add fc00::1/64 dev 6to4_To_KH
 ip link set 6to4_To_KH mtu 1480
 ip link set 6to4_To_KH up
 
-ip -6 tunnel add GRE6Tun_To_KH mode ip6gre remote fde8:b030:25cf::de02 local fde8:b030:25cf::de01
-ip addr add 172.20.20.1/30 dev GRE6Tun_To_KH
-ip link set GRE6Tun_To_KH mtu 1436
-ip link set GRE6Tun_To_KH up
+ip -6 tunnel add ipip6Tun_To_KH mode ipip6 remote fc00::2 local fc00::1
+ip addr add 192.168.13.1/30 dev ipip6Tun_To_KH
+ip link set ipip6Tun_To_KH mtu 1440
+ip link set ipip6Tun_To_KH up
 
 sysctl net.ipv4.ip_forward=1
-iptables -t nat -A PREROUTING -p tcp --dport $ssh_port -j DNAT --to-destination 172.20.20.1
-iptables -t nat -A PREROUTING -j DNAT --to-destination 172.20.20.2
+iptables -t nat -A PREROUTING -p tcp --dport $ssh_port -j DNAT --to-destination 192.168.13.1
+iptables -t nat -A PREROUTING -j DNAT --to-destination 192.168.13.2
 iptables -t nat -A POSTROUTING -j MASQUERADE 
 
 exit 0" > /etc/rc.local
@@ -54,23 +50,19 @@ exit 0" > /etc/rc.local
 		touch /etc/rc.local
 		chmod +x "/etc/rc.local"
 		ip tunnel add 6to4_To_IR mode sit remote $ip_iran local $ip_kharej
-		ip -6 addr add fde8:b030:25cf::de02/64 dev 6to4_To_IR
+		ip -6 addr add fc00::2/64 dev 6to4_To_IR
 		ip link set 6to4_To_IR mtu 1480
 		ip link set 6to4_To_IR up
-		ip -6 tunnel add GRE6Tun_To_IR mode ip6gre remote fde8:b030:25cf::de01 local fde8:b030:25cf::de02
-		ip addr add 172.20.20.2/30 dev GRE6Tun_To_IR
-		ip link set GRE6Tun_To_IR mtu 1436
-		ip link set GRE6Tun_To_IR up
 		echo "#! /bin/bash
 ip tunnel add 6to4_To_IR mode sit remote $ip_iran local $ip_kharej
-ip -6 addr add fde8:b030:25cf::de02/64 dev 6to4_To_IR
+ip -6 addr add fc00::2/64 dev 6to4_To_IR
 ip link set 6to4_To_IR mtu 1480
 ip link set 6to4_To_IR up
 
-ip -6 tunnel add GRE6Tun_To_IR mode ip6gre remote fde8:b030:25cf::de01 local fde8:b030:25cf::de02
-ip addr add 172.20.20.2/30 dev GRE6Tun_To_IR
-ip link set GRE6Tun_To_IR mtu 1436
-ip link set GRE6Tun_To_IR up
+ip -6 tunnel add ipip6Tun_To_IR mode ipip6 remote fc00::1 local fc00::2
+ip addr add 192.168.13.2/30 dev ipip6Tun_To_IR
+ip link set ipip6Tun_To_IR mtu 1440
+ip link set ipip6Tun_To_IR up
 
 exit 0" > /etc/rc.local
 		/etc/rc.local
@@ -79,7 +71,7 @@ exit 0" > /etc/rc.local
 	then 
 		rm -rf /etc/rc.local
 		ip tunnel del 6to4_To_IR
-		ip tunnel del GRE6Tun_To_IR
+		ip tunnel del 6to4_To_KH
 		iptables -F
 		iptables -X
 		iptables -P INPUT ACCEPT
@@ -89,7 +81,7 @@ exit 0" > /etc/rc.local
 	then
 		rm -rf /etc/rc.local
 		ip tunnel del 6to4_To_IR
-		ip tunnel del GRE6Tun_To_IR
+		ip tunnel del 6to4_To_KH
 	else
 		echo "Invalid Input Activity !!"
 	fi
