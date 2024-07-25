d8cfc204719b55c8e539891a0fcdd54eac1aa94e
sudo-sa <162730720+sudo-sa@users.noreply.github.com>
2024-07-25 16:55:04 +0330

    Update-4

--- 6to4script.sh
+++ 6to4script.sh
@@ -1,83 +1,80 @@
 #! /bin/bash
 echo "6to4 Tunnel FOR IRAN SERVER"
 echo ""
-echo "Please Enter IPv4 IRAN : "
+read -p "Please Enter IPv4 IRAN : " ip_iran
 echo ""
-read ip_iran
+read -p "Please Enter IPv4 KHAREJ : " ip_kharej
 echo ""
-echo "Please Enter IPv4 KHAREJ : "
-echo ""
-read ip_kharej
-echo ""
-echo "Please Enter Port SSH : "
-echo ""
-read ssh_port
-echo ""
-echo "Which one to choose:
-
-1 == Tunnel IRAN
-2 == Tunnel KHAREJ
-3 == Remove Tunnel IRAN
-4 == Remove Tunnel Kharej
-
-Enter Number Activity : " 
-echo ""
-read act
-if [ $act -eq 1 ]
+read -p "Please Enter Port SSH : " ssh_port
+if ! [[ $ssh_port =~ ^[0-9]+$ ]]
 then
-	touch /etc/rc.local
-	chmod +x "/etc/rc.local"
-	echo "#! /bin/bash
-ip tunnel add 6to4_To_KH mode sit remote $ip_kharej local $ip_iran
-ip -6 addr add fde8:b030:25cf::de01/64 dev 6to4_To_KH
-ip link set 6to4_To_KH mtu 1480
-ip link set 6to4_To_KH up
-
-ip -6 tunnel add GRE6Tun_To_KH mode ip6gre remote fde8:b030:25cf::de02 local fde8:b030:25cf::de01
-ip addr add 172.20.20.1/30 dev GRE6Tun_To_KH
-ip link set GRE6Tun_To_KH mtu 1436
-ip link set GRE6Tun_To_KH up
-
-sysctl net.ipv4.ip_forward=1
-iptables -t nat -A PREROUTING -p tcp --dport $ssh_port -j DNAT --to-destination 172.20.20.1
-iptables -t nat -A PREROUTING -j DNAT --to-destination 172.20.20.2
-iptables -t nat -A POSTROUTING -j MASQUERADE 
-
-exit 0" > /etc/rc.local
-	/etc/rc.local
-elif [ $act -eq 2 ]
-then
-	touch /etc/rc.local
-	chmod +x "/etc/rc.local"
-	echo "#! /bin/bash
-ip tunnel add 6to4_To_IR mode sit remote $ip_iran local $ip_kharej
-ip -6 addr add fde8:b030:25cf::de02/64 dev 6to4_To_IR
-ip link set 6to4_To_IR mtu 1480
-ip link set 6to4_To_IR up
-
-ip -6 tunnel add GRE6Tun_To_IR mode ip6gre remote fde8:b030:25cf::de01 local fde8:b030:25cf::de02
-ip addr add 172.20.20.2/30 dev GRE6Tun_To_IR
-ip link set GRE6Tun_To_IR mtu 1436
-ip link set GRE6Tun_To_IR up
-
-exit 0" > /etc/rc.local
-	/etc/rc.local
-
-elif [ $act -eq 3 ]
-then 
-	rm -rf /etc/rc.local
-	ip tunnel del 6to4_To_IR
-	ip tunnel del GRE6Tun_To_IR
-	iptables -F
-	iptables -X
-	iptables -P INPUT ACCEPT
-	iptables -P FORWARD ACCEPT
-	iptables -P OUTPUT ACCEPT
-elif [ $act -eq 4 ]
-then
-	rm -rf /etc/rc.local
-	ip tunnel del 6to4_To_IR
-	ip tunnel del GRE6Tun_To_IR
+    echo "Invalid SSH Port !! "
 else
-	echo "Invalid Input Activity !!"
+	echo ""
+	read -p "Which one to choose:
+
+	1 == Tunnel IRAN
+	2 == Tunnel KHAREJ
+	3 == Remove Tunnel IRAN
+	4 == Remove Tunnel Kharej
+
+	Enter Number Activity : " act
+	if [ $act -eq 1 ]
+	then
+		touch /etc/rc.local
+		chmod +x "/etc/rc.local"
+		echo "#! /bin/bash
+	ip tunnel add 6to4_To_KH mode sit remote $ip_kharej local $ip_iran
+	ip -6 addr add fde8:b030:25cf::de01/64 dev 6to4_To_KH
+	ip link set 6to4_To_KH mtu 1480
+	ip link set 6to4_To_KH up
+
+	ip -6 tunnel add GRE6Tun_To_KH mode ip6gre remote fde8:b030:25cf::de02 local fde8:b030:25cf::de01
+	ip addr add 172.20.20.1/30 dev GRE6Tun_To_KH
+	ip link set GRE6Tun_To_KH mtu 1436
+	ip link set GRE6Tun_To_KH up
+
+	sysctl net.ipv4.ip_forward=1
+	iptables -t nat -A PREROUTING -p tcp --dport $ssh_port -j DNAT --to-destination 172.20.20.1
+	iptables -t nat -A PREROUTING -j DNAT --to-destination 172.20.20.2
+	iptables -t nat -A POSTROUTING -j MASQUERADE 
+
+	exit 0" > /etc/rc.local
+		/etc/rc.local
+	elif [ $act -eq 2 ]
+	then
+		touch /etc/rc.local
+		chmod +x "/etc/rc.local"
+		echo "#! /bin/bash
+	ip tunnel add 6to4_To_IR mode sit remote $ip_iran local $ip_kharej
+	ip -6 addr add fde8:b030:25cf::de02/64 dev 6to4_To_IR
+	ip link set 6to4_To_IR mtu 1480
+	ip link set 6to4_To_IR up
+
+	ip -6 tunnel add GRE6Tun_To_IR mode ip6gre remote fde8:b030:25cf::de01 local fde8:b030:25cf::de02
+	ip addr add 172.20.20.2/30 dev GRE6Tun_To_IR
+	ip link set GRE6Tun_To_IR mtu 1436
+	ip link set GRE6Tun_To_IR up
+
+	exit 0" > /etc/rc.local
+		/etc/rc.local
+
+	elif [ $act -eq 3 ]
+	then 
+		rm -rf /etc/rc.local
+		ip tunnel del 6to4_To_IR
+		ip tunnel del GRE6Tun_To_IR
+		iptables -F
+		iptables -X
+		iptables -P INPUT ACCEPT
+		iptables -P FORWARD ACCEPT
+		iptables -P OUTPUT ACCEPT
+	elif [ $act -eq 4 ]
+	then
+		rm -rf /etc/rc.local
+		ip tunnel del 6to4_To_IR
+		ip tunnel del GRE6Tun_To_IR
+	else
+		echo "Invalid Input Activity !!"
+	fi
 fi
