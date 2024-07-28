cff0de2fd0a20372db076ed32827d585149efb7c
sudo-sa <162730720+sudo-sa@users.noreply.github.com>
2024-07-29 01:17:11 +0330

    added-Hybla

--- 6to4script.sh
+++ 6to4script.sh
@@ -1,28 +1,42 @@
 #! /bin/bash
-echo ""
-echo ""
-echo "Angel IV 6to4 Tunnel SCRIPT "
-echo ""
-read -p "Which One To Choose:
+yellow="\e[93m"
+green="\e[92m"
+blue="\e[96m"
+magenta="\e[95m"
+red="\e[91m"
+endcolor="\e[0m"
+bold="\e[1m"
 
-1 == Tunnel IRAN
-2 == Tunnel KHAREJ
-3 == Remove Tunnel IRAN
-4 == Remove Tunnel Kharej
+echo ""
+echo ""
+echo "${magenta}${bold}Angel IV 6to4 Tunnel SCRIPT${endcolor} "
+echo ""
+read -e -p "${green}Which One To Choose:${endcolor}
 
-Enter Number Activity : " act
+${yellow}1${endcolor} == ${blue}Tunnel IRAN${endcolor}
+${yellow}2${endcolor} == ${blue}Tunnel KHAREJ${endcolor}
+${yellow}3${endcolor} == ${blue}Remove Tunnel IRAN${endcolor}
+${yellow}4${endcolor} == ${blue}Remove Tunnel Kharej${endcolor}
+
+${yellow}5${endcolor} == ${blue}Enable Hybla${endcolor}
+${yellow}6${endcolor} == ${blue}Dissable Hybla${endcolor}
+
+${yellow}0${endcolor} == ${blue}Exit${endcolor}
+
+
+${green}Enter Number Activity :${endcolor} " act
 	if [ $act -eq 1 ]
 	then
 		echo ""
-		read -p "Please Enter IPv4 IRAN : " ip_iran
+		read -p "${yellow}Please Enter IPv4 IRAN :${endcolor} " ip_iran
 		echo ""
-		read -p "Please Enter IPv4 KHAREJ : " ip_kharej
+		read -p "${yellow}Please Enter IPv4 KHAREJ${endcolor} : " ip_kharej
 		echo ""
-		read -p "Please Enter Port SSH : " ssh_port
+		read -p "${yellow}Please Enter Port SSH :${endcolor} " ssh_port
 		echo ""
 		if ! [[ $ssh_port =~ ^[0-9]+$ ]]
 		then
-			echo "Invalid SSH Port !! "
+			echo -e "${red}Invalid SSH Port !!${endcolor} "
 		else
 			ip tunnel add 6to4_To_KH mode sit remote $ip_kharej local $ip_iran
 			ip -6 addr add fc00::1/64 dev 6to4_To_KH
@@ -47,7 +61,7 @@ iptables -t nat -A POSTROUTING -j MASQUERADE
 
 exit 0" > /etc/rc.local
 			chmod +x /etc/rc.local
-			echo "Please Configure Other Server And Wait ..."
+			echo "${green}Please Configure Other Server And Wait 1 Minute ...${endcolor}"
 			sleep 1m
 			/etc/rc.local
 		fi
@@ -55,9 +69,9 @@ exit 0" > /etc/rc.local
 	elif [ $act -eq 2 ]
 	then
 		echo ""
-		read -p "Please Enter IPv4 IRAN : " ip_iran
+		read -p "${yellow}Please Enter IPv4 IRAN${endcolor} : " ip_iran
 		echo ""
-		read -p "Please Enter IPv4 KHAREJ : " ip_kharej
+		read -p "${yellow}Please Enter IPv4 KHAREJ${endcolor} : " ip_kharej
 		echo ""
 		ip tunnel add 6to4_To_IR mode sit remote $ip_iran local $ip_kharej
 		ip -6 addr add fc00::2/64 dev 6to4_To_IR
@@ -77,7 +91,7 @@ ip link set ipip6Tun_To_IR up
 
 exit 0" > /etc/rc.local
 		chmod +x /etc/rc.local
-		echo "Please Configure Other Server And Wait ... "
+		echo "${green}Please Configure Other Server And Wait ...${endcolor} "
 		sleep 1m
 		/etc/rc.local
 
@@ -92,13 +106,62 @@ exit 0" > /etc/rc.local
 		iptables -P FORWARD ACCEPT
 		iptables -P OUTPUT ACCEPT
 		echo ""
-		echo "Please Reboot Server !"
+		echo "${green}Please Reboot Server !${endcolor}"
 	elif [ $act -eq 4 ]
 	then
 		rm -rf /etc/rc.local
 		ip tunnel del 6to4_To_IR
 		ip tunnel del ipip6Tun_To_IR
+	elif [ $act -eq 5 ]
+	then
+		kernel="uname -r"
+		echo ""
+		read -p "${blue}To Run Hybla, Your Kernel Version Must Be Higher Than 2.6.13${endcolor}
+${yellow}Your Kernel Version: $kernel${endcolor}
+
+${green}Will The Performance Continue?${endcolor} ${yellow}(default == y)${endcolor} ${green}y\n ${endcolor}" hybla
+		hybla=${hybla:-"y"}
+		if [[ $hybla == "y" ]]
+		then
+			sudo modprobe tcp_hybla
+			sudo sysctl -w net.ipv4.tcp_congestion_control=hybla
+			echo "net.ipv4.tcp_congestion_control=hybla" >> /etc/sysctl.conf
+			sudo sysctl -p
+			if sysctl net.ipv4.tcp_available_congestion_control | grep -q 'hybla';
+			then
+				echo ""
+				echo "${green}Hybla Is Enabled ...${endcolor}"
+			else
+				echo ""
+				echo "${red}Hybla Is Not Enabled !${endcolor}"
+			fi
+		elif [[ $hybla == "n" ]]
+		then
+			bash <(curl -Ls https://raw.githubusercontent.com/Angeliv4/6to4-Tunnel/main/6to4script.sh)
+		else
+			echo ""
+			echo "${red}Invalid Input Activity !!${endcolor}"
+		fi
+	elif [ $act -eq 6 ]
+	then
+		if sysctl net.ipv4.tcp_available_congestion_control | grep -q 'hybla';
+			then
+				sudo sysctl -w net.ipv4.tcp_congestion_control=cubic
+				sudo sed -i 's/net.ipv4.tcp_congestion_control=hybla/net.ipv4.tcp_congestion_control=cubic/' /etc/sysctl.conf
+				sudo sysctl -p
+				echo ""
+				echo "${green}Hybla Is Not Enabled !${endcolor}"
+				sysctl net.ipv4.tcp_available_congestion_control
+			else
+				echo ""
+				echo "${red}Hybla Is Not Enabled !${endcolor}"
+		fi
+	elif [ $act -eq 0 ]
+	then
+		echo ""
+		echo "${magenta}${bold}Good By My Freind .${endcolor}"
+	
 	else
 		echo ""
-		echo "Invalid Input Activity !!"
+		echo "${red}Invalid Input Activity !!${endcolor}"
 	fi
