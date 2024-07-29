3c2ac583e45c125f1632d902f75c6ce7a779f6d9
sudo-sa <162730720+sudo-sa@users.noreply.github.com>
2024-07-29 12:47:32 +0330

    hybla-update-3

--- 6to4script.sh
+++ 6to4script.sh
@@ -112,9 +112,9 @@ exit 0" > /etc/rc.local
 	then
 		kernel=$(uname -r)
 		echo ""
-		read -p "$(echo -e "${blue}To Run Hybla, Your Kernel Version Must Be Higher Than 2.6.13${endcolor}\n\n\
-${yellow}Your Kernel Version: $kernel${endcolor}\n\
-${green}Will The Performance Continue?${endcolor} ${yellow}(default == y)${endcolor}${green}'y\n'${endcolor}")" hybla
+		read -p "$(echo -e "${blue}To Run Hybla, Your Kernel Version Must Be Higher Than 2.6.13${endcolor}\n\
+${yellow}Your Kernel Version: $kernel${endcolor}\n\n\
+${green}Will The Performance Continue?${endcolor} ${yellow}(default == y)${endcolor}${green}`y\n`${endcolor}")" hybla
 		hybla=${hybla:-"y"}
 		if [[ $hybla == "y" ]]
 		then
